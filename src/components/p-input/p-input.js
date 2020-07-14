5efd60a4234eb7f2ecdd0a2595662065aa505edd
leozhang <273274517@qq.comcd>
2020-07-14 11:04:56 +0800

    input提供新的能力

--- src/components/p-input/p-input.js
+++ src/components/p-input/p-input.js
@@ -2,8 +2,48 @@ import './p-input.scss';
 export default{
 	name:'p-input',
 	props:{
-		value:{
+		resize:{
 			type:String,
+			required:false,
+			default:'vertical'
+		},
+		name:{
+			type:String,
+			required:false,
+			default:''
+		},
+		max:{
+			type:Number,
+			required:false,
+			default:-1
+		},
+		minlength:{
+			type:Number,
+			required:false,
+			default:-1
+		},
+		maxlength:{
+			type:Number,
+			required:false,
+			default:-1
+		},
+		min:{
+			type:Number,
+			required:false,
+			default:-1
+		},
+		step:{
+			type:Number,
+			required:false,
+			default:-1
+		},
+		type:{
+			type:String,
+			required:false,
+			default:'text'
+		},
+		value:{
+			type:[String,Number],
 			required:false,
 			default:''
 		},
@@ -31,50 +71,151 @@ export default{
 			required:false,
 			type:String,
 			default:'default'
+		},
+		'prefix-icon':{
+			required:false,
+			type:String,
+			default:''
+		},
+		'suffix-icon':{
+			required:false,
+			type:String,
+			default:''
+		},
+		autofocus:{
+			required:false,
+			type:Boolean,
+			default:false
+		},
+		rows:{
+			required:false,
+			type:Number,
+			default:2
+		},
+		autosize:{
+			reuqired:false,
+			type:[Object,Boolean],
+			default(){
+				return false
+			}
 		}
 	},
 	data(){
 		return{
 			inputStr:'',
 			focused:false,
-			appended:false
+			appended:false,
+			prepended:false,
+			renderRows:0,
 		}
 	},
 	model:{
 		event:'cc',
 		prop:'value'
 	},
+	created(){
+		this.renderRows = this.rows
+	},
+	mounted(){
+		if(this.autosize){
+			let rows = 0;
+			try{
+				rows = this.$refs.textarea.textContent.split('\n').length
+			}catch(e){
+				rows = 1
+				throw(e)
+			}
+			this.$refs.textarea.rows = rows;
+			this.renderRows = rows;
+		}
+	},
 	render(h){
 		let _this = this;
-		let appended = false;
-		if(this.clearable&&this.inputStr.length>0){
-			appended = true;
-		}else{
-			appended = false;
-		}
-		this.appended = appended;
-		return h('div',{
-			class:`p-input p-input-${this.size}`
-		},[
-			this.clearable&&this.inputStr.length>0&&h('i',{
-				class:`fa fa-times-circle-o p-input-clear`,
-				on:{
-					click(){
-						_this.$refs.input.value = '';
-						_this.inputStr = '';
-						_this.$emit('cc','')
-						_this.$emit('clear')
+		
+		let renderInput = () => {
+			let cleared = false;
+			if(_this.clearable&&_this.inputStr.length>0){
+				cleared = true;
+			}else{
+				cleared = false;
+			}
+			if(_this.prefixIcon.trim().length>0){
+				_this.prepended = true;
+			}else{
+				_this.prepended = false;
+			}
+			if(_this.suffixIcon.trim().length>0){
+				_this.appended = true;
+			}else{
+				_this.appended = false;
+			}
+			return [
+				_this.prepended?h('i',{class:`fa ${_this.prefixIcon} p-input-prefix`}):undefined,
+				h('input',{
+					class:`p-input--content ${_this.disabled?'is-disabled':''} ${_this.prepended?'p-input-prepended':''} ${_this.appended||cleared?'p-input-appended':''} p-input-size--${_this.size}`,
+					attrs:{
+						placeholder:_this.placeholder,
+						disabled:_this.disabled?'disabled':undefined,
+						readonly:_this.readonly?'readonly':undefined,
+						autofocus:_this.autofocus?'autofocus':undefined,
+						type:_this.type,
+						value:_this.value,
+						name:_this.name,
+						max:_this.max>-1?_this.max:undefined,
+						min:_this.min>-1?_this.min:undefined,
+						step:_this.step>-1?_this.step:undefined,
+						maxlength:_this.maxlength>-1?_this.maxlength:undefined,
+						minlength:_this.minlength>-1?_this.minlength:undefined,
+					},
+					ref:'input',
+					on:{
+						blur(){
+							_this.$emit('blur',arguments)
+						},
+						focus(){
+							_this.$emit('focus',arguments)
+						},
+						change(){
+							_this.inputStr = arguments[0].target.value
+							_this.$emit('cc',_this.inputStr)
+							_this.$emit('change',_this.inputStr)
+						},
+						input(){
+							_this.inputStr = arguments[0].target.value
+							_this.$emit('cc',_this.inputStr)
+							_this.$emit('input',_this.inputStr)
+						},
+					},
+				},_this.value),
+				cleared?h('i',{
+					class:`fa fa-times-circle-o p-input-clear`,
+					on:{
+						click(){
+							_this.$refs.input.value = '';
+							_this.inputStr = '';
+							_this.$emit('cc','')
+							_this.$emit('clear')
+						}
 					}
-				}
-			}),
-			h('input',{
-				class:`p-input--content ${this.disabled?'is-disabled':''} ${this.appended?'p-input-appended':''} p-input-size--${this.size}`,
+				}):_this.appended?h('i',{
+					class:`fa ${_this.suffixIcon} p-input-suffix`
+				}):undefined
+			]
+		}
+		let renderTextarea = () => {
+			
+			return [h('textarea',{
+				class:`p-textarea--content ${_this.disabled?'is-disabled':''} p-textarea-resize-${_this.resize} p-textarea-size--${_this.size}`,
 				attrs:{
-					placeholder:this.placeholder,
-					disabled:this.disabled?'disabled':undefined,
-					readonly:this.readonly?'readonly':undefined
+					rows:_this.renderRows,
+					placeholder:_this.placeholder,
+					disabled:_this.disabled?'disabled':undefined,
+					readonly:_this.readonly?'readonly':undefined,
+					autofocus:_this.autofocus?'autofocus':undefined,
+					type:_this.type,
+					name:_this.name
 				},
-				ref:'input',
+				ref:'textarea',
 				on:{
 					blur(){
 						_this.$emit('blur',arguments)
@@ -92,8 +233,14 @@ export default{
 						_this.$emit('cc',_this.inputStr)
 						_this.$emit('input',_this.inputStr)
 					},
-				},
-			})
-		])
+					load(){
+						console.log(_this)
+					}
+				}
+			},_this.value)]
+		}
+		return h('div',{
+			class:`${this.type!='textarea'?'p-input':'p-textarea'} p-${this.type!='textarea'?'input':'textarea'}-${_this.size}`
+		},this.type!='textarea'?renderInput():renderTextarea())
 	}
 }
--- src/page/input.vue
+++ src/page/input.vue
@@ -4,19 +4,32 @@
 			<p-link icon="fa-arrow-left" type="primary" router="/">返回</p-link>
 		</div>
 		<div>
-			<p-input @input="handleInput" v-model="str" @clear="handleClear" clearable placeholder="请输入"></p-input>
+			<p-input @input="handleInput" :maxlength="3" v-model="str" @clear="handleClear" clearable placeholder="请输入"></p-input>
 			<div>
 				{{str}}
 			</div>
 		</div>
 		<div>
+			<p-input type="number" v-model="num" clearable :step="10" :min="0" :max="100"></p-input>
+			{{num}}
 			<p-input @input="handleInput" disabled v-model="str" placeholder="请输入"></p-input>
 		</div>
 		<div>
 			<p-input placeholder="请输入" @clear="handleClear" clearable ></p-input>
-			<p-input placeholder="请输入" clearable size="medium"></p-input>
+			<p-input placeholder="请输入" type="password" clearable size="medium"></p-input>
 			<p-input placeholder="请输入" clearable size="small"></p-input>
 			<p-input placeholder="请输入" clearable size="mini"></p-input>
+		</div>
+		<div>
+			<p-input prefix-icon="fa-user" clearable autofocus suffix-icon="fa-user"></p-input>
+		</div>
+		<div style="width: 50%;">
+			<p-input type="textarea" disabled></p-input>
+			<p-input type="textarea" autosize v-model="str1" resize="both" placeholder="请输入"></p-input>
+			{{str1}}
+			<p-input type="textarea" size="medium" placeholder="请输入"></p-input>
+			<p-input type="textarea" size="small" placeholder="请输入"></p-input>
+			<p-input type="textarea" :rows="4" size="mini" placeholder="请输入"></p-input>
 		</div>
 	</div>
 </template>
@@ -25,7 +38,13 @@
 	export default{
 		data(){
 			return {
-				str:''
+				str:'123',
+				str1:`sfjlsdjflksdjklf
+				sdfjklsjdflsdf
+				sdfjklsjdklf
+				sdfjklsdjfklsd
+				sdfjksldf`,
+				num:1
 			}
 		},
 		methods:{
